d1710508f9c89164fa9d7b42fb476dc8dd6ca014
Mardonis Alves <mardonisgp@gmail.com>
2019-02-17 20:41:36 -0300

    validação de acesso

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -6,7 +6,7 @@ use App\Model\User;
 use Doctrine\ORM\EntityManager;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use  Slim\Flash\Messages;
+
 
 class AdminController
 {
@@ -51,14 +51,18 @@ class AdminController
                         $_SESSION['email'] = $l->getEmail();
                         
                         if(isset($_SESSION['email'])){
+
                         $url = $this->container->get('router')->pathFor('home');
                         return $response->withStatus(302)->withHeader('Location', $url); 
+                        
                         }else{
                             echo "A Session não existe" . $_SESSION['email'];
                         }        
                 }else{
-                    $url = $this->container->get('router')->pathFor('index');
-                        return $response->withStatus(302)->withHeader('Location', $url); 
+                    $this->flash->addMessageNow('msg', 'Você não tem Acesso');
+                    $messages = $this->flash->getMessages();
+                    //var_dump($messages);
+                    return $this->container->view->render($response ,'index.twig'  ,Array( 'messages' => $messages)); 
                    
                 }
 
@@ -81,7 +85,7 @@ public function logout($request, $response, $args)
     public function hometeste($request, $response, $args)
     {
        
-        if(isset($_SESSION['email'])){
+        
         $contact = new Contact();
         $this->em->persist($contact);
         $contact->setName($_POST['name']);
@@ -91,9 +95,7 @@ public function logout($request, $response, $args)
         $contact->setPublicationDate(new \DateTime());
         $this->em->flush();
         return $this->container->view->render($response ,'contact.twig');
-        }else{
-            echo "Voce não esta logado";
-        }
+       
         
 }
 
@@ -101,14 +103,14 @@ public function logout($request, $response, $args)
     public function GetcontactID($request, $response, $args)
     {
         $contact =  $this->em->getRepository('App\Model\Contact')->findBy(Array('id' => $_GET['id']));
-        return $this->container->view->render($response ,'updatecontato.twig',Array( 'contact' => $contact));
+        return $this->container->view->render($response ,'admin/updatecontato.twig',Array( 'contact' => $contact));
     }
 
     // Update Contact //
     public function putContact($request, $response, $args)
     {  
-     //echo $_POST['email'];
-       
+  
+       if(isset($_SESSION['email'])){
         $contact =  $this->em->find('App\Model\Contact',$_POST['id']);
         $contact->setEmail($_POST['email']);
         $contact->setTelefone($_POST['telefone']);
@@ -117,13 +119,20 @@ public function logout($request, $response, $args)
     // Retornando o nome da rota 
         $url = $this->container->get('router')->pathFor('home');
         return $response->withStatus(302)->withHeader('Location', $url);
-        
+
+       }else{
+        $this->flash->addMessageNow('msg', 'Você não tem Acesso');
+        $messages = $this->flash->getMessages();
+        //var_dump($messages);
+        return $this->container->view->render($response ,'index.twig'  ,Array( 'messages' => $messages));   
+       }
     }
 
     // Delete Contact //
 
     public function DeleteContact($request, $response, $args)
     {
+        if(isset($_SESSION['email'])){
         $contact =  $this->em->find('App\Model\Contact',$_GET['id']);
         $this->em->remove($contact);
         $this->em->flush();
@@ -131,6 +140,12 @@ public function logout($request, $response, $args)
          // Retornando o nome da rota 
          $url = $this->container->get('router')->pathFor('home');
          return $response->withStatus(302)->withHeader('Location', $url);
+        }else{
+            $this->flash->addMessageNow('msg', 'Você não tem Acesso');
+            $messages = $this->flash->getMessages();
+            //var_dump($messages);
+            return $this->container->view->render($response ,'index.twig'  ,Array( 'messages' => $messages));  
+        }
     }
 
 }
